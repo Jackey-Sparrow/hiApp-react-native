83efc2d9c3779b34be44614f99b7f69ab8d13734
Jackey-Sparrow <1109952026@qq.com>
2015-10-29 21:53:09 +0800

    try to fetch data

--- index.ios.js
+++ index.ios.js
@@ -6,56 +6,79 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    } = React;
 
-var Introduction = [
-  {
-    name:'Jackey',
-    age:26
-  }
-];
+var REQUEST_URL = 'https://raw.githubusercontent.com/Jackey-Sparrow/hiApp-react-native/master/data/userList.json';
 
 var hiAppReactNative = React.createClass({
-  render: function() {
-    var introduction = Introduction[0];
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome!
-        </Text>
-        <Text style={styles.instructions}>
-          {introduction.name}
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    getInitialState: function () {
+        return {
+            userList: null
+        };
+    },
+    fetchData: function () {
+        var that = this;
+        fetch(REQUEST_URL).then(function (response) {
+            return response.json();
+        }).then(function (responseData) {
+            that.setState({
+                userList: responseData
+            });
+        }).done();
+    },
+    componentDidMount: function () {
+        this.fetchData();
+    },
+    renderLoadingView: function () {
+        return (
+            <View style={styles.container}>
+                <Text>loading...</Text>
+            </View>
+        );
+    },
+    renderUser: function (user) {
+        return (
+            <View style={styles.container}>
+                <Image source={{uri:user.photoUrl}}/>
+                <View style={styles.rightContainer}>
+                    <Text>{user.userName}</Text>
+                    <Text>{user.age}</Text>
+                </View>
+            </View>
+        );
+    },
+    render: function () {
+        if(!this.state.userList){
+            return this.renderLoadingView();
+        }
+
+        var user = this.state.userList[0];
+        return this.renderUser(user);
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('hiAppReactNative', () => hiAppReactNative);
